857c1f846b7ec7756d7d7b1b85bd7a61ff46f69b
Tenjy-x <tendrymk848@gmail.com>
2026-07-02 10:46:23 +0300

    functionSum

--- src/main/java/com/volyVary/repository/LotPaddyMouvementRepository.java
+++ src/main/java/com/volyVary/repository/LotPaddyMouvementRepository.java
@@ -1,11 +1,13 @@
 package com.volyVary.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.volyVary.modele.LotPaddyMouvement;
 
 @Repository
 public interface LotPaddyMouvementRepository extends JpaRepository<LotPaddyMouvement , Integer>{
-    
+    @Query("SELECT SUM(l.quantite) FROM lot_paddy_mouvement l")
+    Double sommeQuantiteLotPaddyMouvement();
 }
--- src/main/java/com/volyVary/service/TransformationService.java
+++ src/main/java/com/volyVary/service/TransformationService.java
@@ -7,6 +7,7 @@ import com.volyVary.repository.DetailLotTransformeRepository;
 import com.volyVary.repository.LotPaddyRepository;
 import com.volyVary.repository.LotPaddyTransformeRepository;
 import com.volyVary.repository.ProduitRepository;
+import com.volyVary.repository.LotPaddyMouvementRepository;
 
 @Service
 public class TransformationService {
@@ -22,13 +23,24 @@ public class TransformationService {
     @Autowired
     private ProduitRepository produitRepository;
 
+    @Autowired
+    private LotPaddyMouvementRepository lotPaddyMouvementRepository;
+
     public TransformationService(DetailLotTransformeRepository detailLotTransformeRepository,
             LotPaddyRepository lotPaddyRepository, LotPaddyTransformeRepository lotPaddyTransformeRepository,
-            ProduitRepository produitRepository) {
+            ProduitRepository produitRepository, LotPaddyMouvementRepository lotPaddyMouvementRepository) {
         this.detailLotTransformeRepository = detailLotTransformeRepository;
         this.lotPaddyRepository = lotPaddyRepository;
         this.lotPaddyTransformeRepository = lotPaddyTransformeRepository;
         this.produitRepository = produitRepository;
+        this.lotPaddyMouvementRepository = lotPaddyMouvementRepository;
     }
-    
+
+    public Double getTotalQuantiteLotPaddy() {
+        return lotPaddyRepository.sommeQuantite();
+    }
+
+    public Double getTotalQuantiteLotPaddyMouvement() {
+        return lotPaddyMouvementRepository.sommeQuantiteLotPaddyMouvement() != null ? lotPaddyMouvementRepository.sommeQuantiteLotPaddyMouvement() : 0.0;
+    }
 }
